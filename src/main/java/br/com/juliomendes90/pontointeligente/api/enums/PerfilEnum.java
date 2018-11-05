4bbd94406a5a95a35319bc536427a493ffdb7326
Júlio <julio.mendes@sensedia.com>
2018-11-05 19:36:26 -0200

    Criação das entidades e das configurações do Flyway

---
+++ src/main/java/br/com/juliomendes90/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package br.com.juliomendes90.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS `empresa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empresa` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `razao_social` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `funcionario` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `perfil` varchar(255) NOT NULL,
+  `qtd_horas_almoco` float DEFAULT NULL,
+  `qtd_horas_trabalho_dia` float DEFAULT NULL,
+  `senha` varchar(255) NOT NULL,
+  `valor_hora` decimal(19,2) DEFAULT NULL,
+  `empresa_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK4cm1kg523jlopyexjbmi6y54j` (`empresa_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `lacamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lacamento` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `data` datetime NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `localizacao` varchar(255) DEFAULT NULL,
+  `tipo_lancamento` varchar(255) NOT NULL,
+  `funcionario_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKl92j1uho6et6gabiooqeekuyy` (`funcionario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
